0b30a56ee4084297221a0b4f9d6b35694b4ac0cb
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-04-30 15:44:38 +0300

    feat: user validation to rent car

--- src/cars/services/user-rent-cars.service.ts
+++ src/cars/services/user-rent-cars.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Repositories } from 'src/database/database.model.repositories';
-import { UserRentCar } from '../models/user-rent-car';
+import { UserRentCar } from '../models/user-rent-car.model';
 import { UserRentCarsInput } from '../input/user-rent-car.input';
 import { Car } from '../models/car.model';
 import { BaseHttpException } from 'src/exceptions/base-http-exception';
@@ -19,17 +19,25 @@ export class UserRentCarService {
     ) { }
 
     async createCarBoard(input: UserRentCarsInput) {
+
         const car = await this.carRepo.findOne({ where: { id: input.carId } });
         if (!car) throw new BaseHttpException(ErrorCodeEnum.CAN_NOT_FIND_CAR);
-        const user = await this.userRepo.findOne({ where: { id: input.carId } });
-        if (!user) throw new BaseHttpException(ErrorCodeEnum.CAN_NOT_FIND_CAR);
+
+        const user = await this.userRepo.findOne({ where: { id: input.userId } });
+        if (!user) throw new BaseHttpException(ErrorCodeEnum.INVALID_USER);
+
         if (input.startDate > input.endDate) throw new BaseHttpException(ErrorCodeEnum.START_DATE_MUST_BE_SMALLER_THAN_END_DATE);
+
         const rentCar = await this.userRentCarRepo.findAll({ where: { carId: input.carId } });
         rentCar.map(car => {
-            if (car.endDate.valueOf() > input.startDate) throw new BaseHttpException(ErrorCodeEnum.THIS_CAR_ALREADY_RENT_DOR_NOW);
+            if (car.endDate.valueOf() >= input.startDate) throw new BaseHttpException(ErrorCodeEnum.THIS_CAR_ALREADY_RENT_DOR_NOW);
+            const prepPeriod = car.endDate.valueOf() + 86400000;
+            if (prepPeriod >= input.startDate) throw new BaseHttpException(ErrorCodeEnum.CAR_IN_PREP_PERIOD_NOW_AND_WILL_VALID_IN_24_HOURS);
         });
+
         const rentPeriod = ((input.endDate - input.startDate) / 1000 / 60 / 60 / 24) + 1;
         const totalPrice = car.rentPricePerDay * rentPeriod;
+
         return await this.userRentCarRepo.create({
             ...input,
             period: rentPeriod,
