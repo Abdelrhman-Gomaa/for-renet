06cb9c83441093713cdf1412f09c5cec6b4080b1
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-05-01 11:53:31 +0300

    feat: graphql configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,19 @@ import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { CarModule } from './cars/car.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'schema.gql'),
+      sortSchema: true,
     }),
     DatabaseModule,
     UserModule,
