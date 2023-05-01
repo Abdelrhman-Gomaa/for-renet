cdcb29c81e1ae7c242cd942b6956b197c7b0e441
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-05-01 18:29:26 +0300

    fix: input validation

--- src/cars/input/user-rent-car.input.ts
+++ src/cars/input/user-rent-car.input.ts
@@ -1,31 +1,39 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsNumber, IsUUID, Min } from 'class-validator';
 import { RentTypeEnum } from '../car.enum';
+import { Field, ID, InputType } from '@nestjs/graphql';
 
+@InputType()
 export class UserRentCarsInput {
-    
+
     @IsNotEmpty()
-    @IsString()
+    @IsUUID()
     @ApiProperty()
+    @Field()
     carId: string;
 
     @IsNotEmpty()
-    @IsString()
+    @IsUUID()
     @ApiProperty()
+    @Field()
     userId: string;
 
     @IsNotEmpty()
     @IsNumber()
+    @Min(new Date().valueOf())
     @ApiProperty()
+    @Field()
     startDate: number;
 
     @IsNotEmpty()
     @IsNumber()
     @ApiProperty()
+    @Field()
     period: number;
 
     @IsNotEmpty()
     @IsEnum(RentTypeEnum)
     @ApiProperty({ enum: RentTypeEnum })
+    @Field(() => RentTypeEnum)
     rentType: RentTypeEnum;
 }
