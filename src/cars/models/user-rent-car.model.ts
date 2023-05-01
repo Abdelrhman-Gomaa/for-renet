b9e04b294f98a8cbbfb939cb2f2fd467b2d08669
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-05-01 18:30:19 +0300

    fix: add grapgqh syntax

--- src/cars/models/user-rent-car.model.ts
+++ src/cars/models/user-rent-car.model.ts
@@ -13,19 +13,23 @@ import {
 import { User } from 'src/user/models/user.model';
 import { Car } from './car.model';
 import { RentTypeEnum } from '../car.enum';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 
 @Table
+@ObjectType()
 export class UserRentCar extends Model {
     @PrimaryKey
     @Default(DataType.UUIDV4)
     @Column({ type: DataType.UUID })
     @ApiProperty()
+    @Field(() => ID)
     id: string;
 
     @ForeignKey(() => User)
     @AllowNull(false)
     @Column({ type: DataType.UUID, onDelete: 'CASCADE', onUpdate: 'CASCADE' })
     @ApiProperty()
+    @Field()
     userId: string;
 
     @BelongsTo(() => User)
@@ -35,6 +39,7 @@ export class UserRentCar extends Model {
     @AllowNull(false)
     @Column({ type: DataType.UUID, onDelete: 'CASCADE', onUpdate: 'CASCADE' })
     @ApiProperty()
+    @Field()
     carId: string;
 
     @BelongsTo(() => Car)
@@ -43,23 +48,28 @@ export class UserRentCar extends Model {
     @AllowNull(false)
     @Column({ type: DataType.DATE })
     @ApiProperty()
+    @Field()
     startDate: Date;
 
     @AllowNull(false)
     @Column({ type: DataType.DATE })
     @ApiProperty()
+    @Field()
     endDate: Date;
 
     @AllowNull(false)
     @Column({ type: DataType.DOUBLE })
+    @Field()
     totalPrice: number;
 
     @AllowNull(false)
     @Column({ type: DataType.INTEGER })
+    @Field()
     period: number;
 
     @ApiProperty()
     @AllowNull(false)
     @Column({ type: DataType.ENUM(...Object.values(RentTypeEnum)) })
+    @Field(() => RentTypeEnum)
     rentType: RentTypeEnum;
 }
